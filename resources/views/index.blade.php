7e23ac8c1e32f323729dbdace6bdc1ff8c91f65d
YueXingTong <d00234628@student.dkit.ie>
2022-04-24 00:48:47 +0100

    Added edit and delete feature in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,6 +42,59 @@
             These Are The 4 Latest Posts
         </h2>
     </div>
+
+    @foreach ($posts as $post)
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="{{ asset('images/' . $post->image_path) }}" alt="">
+        </div>
+        <div>
+            <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                {{ $post->title }}
+            </h2>
+
+            <span class="text-gray-500">
+                By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+            </span>
+
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                {{ $post->description }}
+            </p>
+
+            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                Keep Reading
+            </a>
+
+            @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                <span class="float-right">
+                    <a 
+                        href="/blog/{{ $post->slug }}/edit"
+                        class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
+                        Edit
+                    </a>
+                </span>
+
+                <span class="float-right">
+                     <form 
+                        action="/blog/{{ $post->slug }}"
+                        method="POST">
+                        @csrf
+                        @method('delete')
+
+                        <button
+                            class="text-red-500 pr-3"
+                            type="submit">
+                            Delete
+                        </button>
+
+                    </form>
+                </span>
+            @endif
+        </div>
+    </div>    
+@endforeach
+
+
     {{-- <span>{{ Auth::user()->name }}</span> --}}
     <div class="sm:grid grid-cols-2">
         <div class="sm:grid grid w-3/4 m-auto mt-11">
